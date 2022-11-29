f8dab4b35f4db2f445ec9d594515abc2f1610372
Prayoga Volantis <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2022-11-29 18:26:27 +0700

    Backend-Master

---
+++ api/user.go
@@ -0,0 +1,68 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lib/pq"
+	"net/http"
+	"time"
+	db "tutorial.sqlc.dev/app/db/sqlc"
+	"tutorial.sqlc.dev/app/util"
+)
+
+type createUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+	Fullname string `json:"fullname" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+type createUserResponse struct {
+	Username         string    `json:"username"`
+	Fullname         string    `json:"fullname"`
+	Email            string    `json:"email"`
+	PasswordChangeAt time.Time `json:"password_change_at"`
+	CreatedAt        time.Time `json:"created_at"`
+}
+
+func (server *Server) createUser(ctx *gin.Context) {
+	var req createUserRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	arg := db.CreateUserParams{
+		Username:       req.Username,
+		HashedPassword: hashedPassword,
+		Fullname:       req.Fullname,
+		Email:          req.Email,
+	}
+	user, err := server.store.CreateUser(ctx, arg)
+	if err != nil {
+		if pgErr, ok := err.(*pq.Error); ok {
+			switch pgErr.Code.Name() {
+			case "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	response := createUserResponse{
+		Username:         user.Username,
+		Fullname:         user.Fullname,
+		Email:            user.Email,
+		PasswordChangeAt: user.PasswordChangeAt,
+		CreatedAt:        user.CreatedAt,
+	}
+
+	ctx.JSON(http.StatusOK, response)
+}
